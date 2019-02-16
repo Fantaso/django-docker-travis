8bb620262f5f517d4bf73c2834f7f1d78cf22623
fantaso <fantaso.code@gmail.com>
2019-02-16 17:49:13 +0100

    added the database configurations variables

--- wine_ecommerce/wine_ecommerce/settings.py
+++ wine_ecommerce/wine_ecommerce/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'wine_ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wine_ecommerce_db',
+        'USER': 'wine-admin',
+        'PASSWORD': 'wine-secret',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
