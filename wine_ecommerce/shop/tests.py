0f05126b9cfa1e72ed2b6f659151e2a33e5609e1
fantaso <fantaso.code@gmail.com>
2019-02-18 19:35:31 +0100

    added 2 tests for the app

--- wine_ecommerce/shop/tests.py
+++ wine_ecommerce/shop/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class ClientTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+
+    def test_client_ok_response(self):
+        # make the request
+        response = self.client.get('/shop')
+
+        # assert that the web page loads correctly
+        self.assertEqual(response.status_code, 200)
+
+    def test_client_false_response(self):
+        # make the request
+        response = self.client.get('/shop')
+
+        # assert that the web page loads correctly
+        self.assertNotEqual(response.status_code, 400)
