7a5c44b5ea77450879bb8ccc9fb243ef876e0fce
fantaso <fantaso.code@gmail.com>
2019-02-18 19:30:09 +0100

    added new view, confifure urls, added html templates

--- wine_ecommerce/shop/views.py
+++ wine_ecommerce/shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+
+    num4 = 10
+    context = {
+        'nums': [7,17,27],
+        "nums2": {
+            'num1':2,
+            'num2':3,
+            'num3':4,
+        }
+    }
+
+    return render(request, "shop/home.html", context, num4)
